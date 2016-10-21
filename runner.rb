e967328266a8df96d2fae3d3416196347f88ad03
Arshdeep Singh & Miles McArdle-Coe & Lindsay Kelly <pair+arsy209.kndekaru.lindsaymkelly@devbootcamp.com>
2016-10-21 10:37:40 -0400

    Add initial files for water quality incidents

---
+++ runner.rb
@@ -0,0 +1,5 @@
+require 'open-uri'
+require 'json'
+require_relative 'water_quality'
+puts "Please enter your zip code"
+user_response_zip = gets.chomp
---
+++ water_quality.rb
@@ -0,0 +1,9 @@
+require 'open-uri'
+require 'json'
+class WaterQualityController
+BASE_URL = "https://data.cityofnewyork.us/resource/qfe3-6dkn.json"
+
+ZIP_ID_QUERY = "incident_zip="
+
+DESCRIPTOR_QUERY = "descriptor="
+end
