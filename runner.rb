25e3cd825649c8b22ffedda10f87099825ec0fed
Arshdeep Singh & Miles McArdle-Coe & Lindsay Kelly <pair+arsy209.kndekaru.lindsaymkelly@devbootcamp.com>
2016-10-21 15:18:38 -0400

    Simplify runner code to essentials

--- runner.rb
+++ runner.rb
@@ -4,9 +4,9 @@ require_relative 'info_displayable'
 require_relative 'controller'
 require_relative 'model'
 
-test = WaterQualityController.new(:incidents => [])
+demo = WaterQualityController.new(:incidents => [])
 
-test.run
-test.display
+demo.run
+
 
 
